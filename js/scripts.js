1ac33bac9a7336acb4cd06214d11e6db376936c6
Mercy Rigua <mercyrigua@gmail.com>
2018-05-04 14:40:17 +0300

    ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 
 var countTonumber;
-$(document).ready(function(){
+var str='';
+condition();
 
-$('form.pingp').submit(function(){
-countTonumber=parseInt($("#in").val());
-for(var i=1;i<=countTonumber;i++){
-if((i % 3==0) && (i % 5==0)){
-$(".list").append('<li>pingpong</li>');
-}
-else if(i % 5==0){
-$(".list").append('<li>pong</li>');
-}
-else if(i % 3==0){
-$(".list").append('<li>ping</li>');
-}
-else{
-  $(".list").append('<li>'+i.toString() + '</li>');
-}
-}
-$("#in").val('')
-event.preventDefault();
-});
-});
+
+function condition(){
+ for(var i=1;i<=countTonumber;i++){
+   if((i % 3==0) && (i % 5==0)){
+    str+='pingpong';
+   }
+   else if(i % 5==0){
+    str+='pong';
+   }
+    else if(i % 3==0){
+    str+='ping';
+   }
+   else{
+      str+=i.toString();
+   }
+   if(i!=countTonumber){
+    str+=',';
+   }
+
+ }
+    
