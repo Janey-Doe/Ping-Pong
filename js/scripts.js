f2f4e1ee7fadf5ae1a0b85079f2b79aafdf0bf58
Mercy Rigua <mercyrigua@gmail.com>
2018-05-06 03:16:26 +0300

    jumbotron image change

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
 
-var countTonumber;
-var str='';
-condition();
+var output = "";
+var num ;
+function getResponse(play){
+    if (play % 5 ===0 && play % 3 === 0) {
+            output = "Pingpong"
+    } else if (play % 5 === 0) {
+        output = "Pong"
+    } else if (play % 3 === 0) {
+        output = "Ping"
+    } else {
+        output = play;}
+    return output;
+}
 
+$(document).ready(function(){
+    $("form#pingp").submit(function (event) {
+      $("ul#list-1").text('');
+        num = parseInt($("#in").val());
+        for(var counter =1;counter <=num; counter ++ ) {
+            var result = getResponse(counter)
+            $("ul#list-1").append("<li>"+result+"</li>")
+        }
+        $("#list-1").show();
+        $("#in").val("");
+        event.preventDefault();
+    });
 
-function condition(){
- for(var i=1;i<=countTonumber;i++){
-   if((i % 3==0) && (i % 5==0)){
-    str+='pingpong';
-   }
-   else if(i % 5==0){
-    str+='pong';
-   }
-    else if(i % 3==0){
-    str+='ping';
-   }
-   else{
-      str+=i.toString();
-   }
-   if(i!=countTonumber){
-    str+=',';
-   }
-
- }
-    
+});
